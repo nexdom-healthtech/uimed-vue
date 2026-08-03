5e7a6607e98c80107699ff55cc0babbef958e7ac
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-03 17:26:15 +0000

    feat(components): add root component

---
+++ src/components/root.vue
@@ -0,0 +1,13 @@
+<template>
+  <slot />
+</template>
+
+<script lang="ts">
+ /**
+ * Root component to be placed at the top of the component tree.
+ */
+</script>
+
+<script setup lang="ts">
+import "vuetify/styles";
+</script>
