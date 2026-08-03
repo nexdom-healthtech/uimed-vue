6feb5bcc6b88c4ff19d83697433ab673aa518c68
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-03 17:25:44 +0000

    chore: update configs to handle Vue UI Components

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -3,6 +3,25 @@ module.exports = {
   extends: "dependency-cruiser/configs/recommended-strict",
   forbidden: [
     {
+      name: "no-duplicate-dep-types",
+      comment:
+        "Likely this module depends on an external ('npm') package that occurs more " +
+        "than once in your package.json i.e. bot as a devDependencies and in dependencies. " +
+        "This will cause maintenance problems later on. If it's intentional, you can " +
+        "disable this rule by adding this override as a rule in the 'forbidden' section " +
+        "of your dependency-cruiser configuration: " +
+        '{"name": "no-duplicate-dep-types", "severity": "ignore"}',
+      severity: "warn",
+      from: {},
+      to: {
+        moreThanOneDependencyType: true,
+        // as it's pretty common to have a type import be a type only import
+        // _and_ (e.g.) a devDependency - don't consider type-only dependency
+        // types for this rule
+        dependencyTypesNot: ["type-only", "npm-peer"],
+      },
+    },
+    {
       name: "no-orphans",
       comment:
         "This is an orphan module - it's likely not used (anymore?). Either use it or " +
