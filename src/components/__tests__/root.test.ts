129dd079fa633ff627aa6cc2b66746129abdb613
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-03 20:55:32 +0000

    test(components): validate root

--- src/components/__tests__/root.test.ts
+++ src/components/__tests__/root.test.ts
@@ -1,9 +1,25 @@
+import { VApp } from "vuetify/components";
 import Root from "@/components/root.vue";
 import { mount } from "@vue/test-utils";
 
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({ components, directives });
+
 describe("root", () => {
+  const wrapper = mount(Root, {
+    global: {
+      plugins: [vuetify],
+    },
+  });
+
   it("should exists", () => {
-    const wrapper = mount(Root);
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it("should contain primary component", () => {
+    expect(wrapper.findComponent(VApp).exists()).toBe(true);
   });
 });
