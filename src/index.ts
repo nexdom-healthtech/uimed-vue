987cc9bf9acbc8d8ddab25ded6df2104bda2ccb6
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-03 20:55:04 +0000

    feat: add createUimed API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+
+/* optional italic styles */
+import "@fontsource/roboto/100-italic.css";
+import "@fontsource/roboto/300-italic.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/500-italic.css";
+import "@fontsource/roboto/700-italic.css";
+import "@fontsource/roboto/900-italic.css";
+
+import "@mdi/font/css/materialdesignicons.css";
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+
+/**
+ * Create an uimed-vue instance to be installed after [createApp](https://vuejs.org/guide/essentials/application.html#the-application-instance).
+ * @returns an instance to be used with [app.use](https://vuejs.org/guide/essentials/application.html#the-application-instance)
+ */
+export const createUimed = () => createVuetify({ theme: { defaultTheme: "light" } });
+
 export * as components from "@/components/index.ts";
