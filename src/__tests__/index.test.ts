f75af251a183d64cfeed42178e49147b326bea2a
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-03 17:26:27 +0000

    test(components): validate root component

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,13 @@
-import { expect, test } from "vite-plus/test";
-import { sayHello } from "@/index.ts";
+// import { components, composables } from "@/index.ts";
+import { components } from "@/index.ts";
 
-test("sayHello", () => {
-  expect(sayHello()).toBe("Hello, NEXTERS!");
+describe("index", () => {
+  it("should export components", () => {
+    expect(components).not.toBeUndefined();
+  });
+
+  // it.todo("should export composables", () => {
+  //   expect(composables).not.toBe(components);
+  //   expect(composables).not.toBeUndefined();
+  // });
 });
