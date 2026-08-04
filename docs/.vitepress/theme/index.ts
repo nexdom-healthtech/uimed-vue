45f6ee96fd21e37559b942f5b31bada2526a38ae
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-04 16:24:38 +0000

    docs: update infos about this project, how to use and contribute

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import "virtual:group-icons.css";
 import "./style.css";
 
 import { createUimed } from "../../../dist/index.js";
