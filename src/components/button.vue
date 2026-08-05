b3dab33e74d962c310024d5331dfcf84f17f5a9a
Douglas Martins <douglasfabiamartins@hotmail.com>
2026-08-05 21:45:50 +0000

    feat(components): add button component

---
+++ src/components/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <VBtn> </VBtn>
+</template>
+
+<script lang="ts">
+ /**
+ * Button component to be used throughout the application.
+ */
+</script>
+
+<script setup lang="ts">
+import { VBtn } from "vuetify/components";
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
