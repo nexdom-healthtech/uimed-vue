0f0cb86e026ba0cbb8c540c7902961806d8a712e
Douglas Martins <douglasfabiamartins@hotmail.com>
2026-08-15 00:39:15 +0000

    feat(btn): renomeia Button para Btn e redesenha as props variant e color

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
 export { default as Root } from "@/components/root.vue";
-export { default as Button } from "@/components/button/button.vue";
+export { default as Btn } from "@/components/btn/btn.vue";
