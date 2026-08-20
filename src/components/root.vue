772839ec1e35d7bc225774898c5c8fa692153398
Douglas Martins <douglasfabiamartins@hotmail.com>
2026-08-20 19:57:52 +0000

    refactor(root): substitui defineOptions por export default clássico

--- src/components/root.vue
+++ src/components/root.vue
@@ -5,15 +5,14 @@
 </template>
 
 <script lang="ts">
- /**
+/**
  * Root component to be placed at the top of the component tree.
  */
+export default {
+  inheritAttrs: false,
+};
 </script>
 
 <script setup lang="ts">
 import { VApp } from "vuetify/components";
-
-defineOptions({
-  inheritAttrs: false,
-});
 </script>
