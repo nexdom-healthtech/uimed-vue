f8d7fb3f12bffa3a5739f68deed97f082aa58993
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-26 20:45:47 +0000

    feat(unit-test): add Vitest server plugin and @vue/test-utils plugin

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,8 +1,16 @@
 import vitePluginVuetify from "vite-plugin-vuetify";
+import type { UserConfig } from "vite-plus";
 
 /**
  * Vite plugin for Uimed.
  */
 export function vitePluginUimed(): ReturnType<typeof vitePluginVuetify> {
   return vitePluginVuetify({ autoImport: false });
+}
+
+/**
+ * Vitest Server plugin for Uimed.
+ */
+export function vitestServerPluginUimed(): NonNullable<NonNullable<UserConfig["test"]>["server"]> {
+  return { deps: { inline: ["vuetify"] } };
 }
