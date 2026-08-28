a3f9c3f961d5cddc23b73147fa85ea081118ef75
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-28 20:11:43 +0000

    feat(components): add TextField component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Root } from "@/components/root/root.vue";
 export { default as Btn } from "@/components/btn/btn.vue";
+export { default as TextField } from "@/components/text-field/text-field.vue";
