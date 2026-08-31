3481f5557fc88186caddfe7f2a8ada43f5d34f5e
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-08-31 13:50:47 +0000

    docs: improve Playground to use uimed-vue TextField component

--- docs/.vitepress/theme/components/playground.vue
+++ docs/.vitepress/theme/components/playground.vue
@@ -4,10 +4,37 @@
       <slot />
     </div>
     <div class="playground-controls">
+      <template v-for="[index, action] in Object.entries(actions)">
+        <text-field
+          :model-value="action.value"
+          :label="action.label"
+          :data-testid="action.dataTestid"
+          @update:model-value="updateActions(index, $event)"
+        />
+      </template>
       <slot name="actions" />
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { TextField } from "../../../../dist/components.js";
+
+interface Input {
+  type: "text";
+  value: string;
+  label: string;
+  dataTestid: string;
+}
+
+const actions = defineModel<Record<string, Input>>("actions", { default: () => ({}) });
+
+function updateActions(index: keyof typeof actions.value, newValue: string) {
+  const action = actions.value[index];
+  const newAction = { ...action, value: newValue };
+  actions.value = { ...actions.value, [index]: newAction };
+}
+</script>
 
 <style lang="scss" scoped>
 .playground {
