07a48ef9dc9dd3512630b8d7e4e5b347a09c0b57
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-09-09 12:33:54 +0000

    feat(composables): add `useToast` composable

--- src/composables/colors/types.ts
+++ src/composables/colors/types.ts
@@ -1,7 +1,5 @@
-export type ColorVariant =
-  | "primary"
-  | "secondary"
-  | "positive"
-  | "informative"
-  | "caution"
-  | "danger";
+export type ColorVariant = "primary" | "secondary" | FeedbackColorVariant;
+
+export type FeedbackColorVariant = "positive" | "informative" | "caution" | "danger";
+
+export type VuetifyColor = "primary" | "secondary" | "success" | "info" | "warning" | "error";
