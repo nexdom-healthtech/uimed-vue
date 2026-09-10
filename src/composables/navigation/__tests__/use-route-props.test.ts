a425fd0ea9076b97340ab750b1d1298552c8e658
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-09-10 17:51:55 +0000

    test(composables): adjust `useRouteProps` typing

--- src/composables/navigation/__tests__/use-route-props.test.ts
+++ src/composables/navigation/__tests__/use-route-props.test.ts
@@ -1,28 +1,28 @@
 import useRouteProps from "@/composables/navigation/use-route-props.ts";
-import { ref, toValue } from "vue";
+import { ref } from "vue";
 
 describe("useRouteProps", () => {
   it("shouldn't return `to` and `href` as undefined when provided prop is undefined", () => {
     const help = ref();
-    const { href, to } = useRouteProps(help).value;
+    const props = useRouteProps(help);
 
-    expect(to).toBeUndefined();
-    expect(href).toBeUndefined();
+    expect(props.value).toHaveProperty("to", help.value);
+    expect(props.value).not.toHaveProperty("href");
   });
 
   it("should only return `to` when vue router route is provided", () => {
     const help = ref({ path: "/help" });
-    const { href, to } = useRouteProps(help).value;
+    const props = useRouteProps(help);
 
-    expect(to).toEqual(toValue(help));
-    expect(href).toBeUndefined();
+    expect(props.value).toHaveProperty("to", help.value);
+    expect(props.value).not.toHaveProperty("href");
   });
 
   it("should only return `href` when external route is provided", () => {
     const help = ref("https://localhost:8080/help");
-    const { href, to } = useRouteProps(help).value;
+    const props = useRouteProps(help);
 
-    expect(to).toBeUndefined();
-    expect(href).toEqual(toValue(help));
+    expect(props.value).not.toHaveProperty("to");
+    expect(props.value).toHaveProperty("href", help.value);
   });
 });
