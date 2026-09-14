03122ac8047634156d7d6d2a7b06dd7fe8add459
rafael.perini@nexdom.tec.br <rafael.perini@nexdom.tec.br>
2026-09-14 16:10:16 +0000

    feat(components): list notifications by `Root`'s app bar and exposed by `notificationsOpen` model

--- src/components/app-bar/app-bar.vue
+++ src/components/app-bar/app-bar.vue
@@ -7,15 +7,48 @@
         :data-testid="helpBtnDataTestid"
         icon="mdi-help-circle-outline"
       />
+      <v-menu v-if="showNotifications" v-model="notificationsOpen" :close-on-content-click="false">
+        <template #activator="{ props }">
+          <v-btn v-bind="props" :data-testid="notificationsBtnDataTestid" icon>
+            <v-badge :model-value="hasUnreadNotifications" :content="unreadCount" color="error">
+              <v-icon icon="mdi-bell-outline" />
+            </v-badge>
+          </v-btn>
+        </template>
+
+        <v-card :data-testid="notificationsMenuDataTestid">
+          <v-list v-if="props.notifications?.length">
+            <v-list-subheader>Notificações</v-list-subheader>
+
+            <template v-for="(notification, index) in props.notifications" :key="index">
+              <v-list-item
+                :active="!notification.read"
+                :title="notification.title"
+                :subtitle="notification.subtitle"
+                active-class="text-primary"
+              >
+                <template v-if="notification.date" #append>
+                  <v-list-item-action end>
+                    <small class="opacity-60">
+                      {{ formatNotificationDate(notification.date) }}
+                    </small>
+                  </v-list-item-action>
+                </template>
+              </v-list-item>
+            </template>
+          </v-list>
+          <v-card-text v-else>Nenhuma notificação</v-card-text>
+        </v-card>
+      </v-menu>
       <v-menu v-if="showUser" :close-on-content-click="false">
-        <template v-slot:activator="{ props }">
+        <template #activator="{ props }">
           <v-btn v-bind="props" :data-testid="userBtnDataTestid" icon>
             <v-avatar v-bind="userAvatarProps" color="primary" />
           </v-btn>
         </template>
 
         <v-card :title="user?.title" :subtitle="user?.subtitle" :data-testid="userMenuDataTestid">
-          <template v-slot:prepend>
+          <template #prepend>
             <v-avatar v-bind="userAvatarProps" color="primary" />
           </template>
 
@@ -27,7 +60,7 @@
 
               <template v-if="isParentOption(option)">
                 <v-list-group>
-                  <template v-slot:activator="{ props }">
+                  <template #activator="{ props }">
                     <v-list-item v-bind="props" :title="option.description" />
                   </template>
 
@@ -68,27 +101,42 @@ import {
   type ParentOptionOrOption,
 } from "@/components/app-bar/types.ts";
 import useRouteProps, { routeToProps } from "@/composables/navigation/use-route-props.ts";
+import { formatDateTime, toPeriodInterval } from "@nexdom/shared/utils";
 import { computed } from "vue";
 import {
   VAppBar,
   VBtn,
   VMenu,
   VCard,
+  VCardText,
   VAvatar,
+  VBadge,
+  VIcon,
   VDivider,
   VList,
   VListItem,
   VListGroup,
+  VListSubheader,
+  VListItemAction,
 } from "vuetify/components";
 
 const props = defineProps<AppBarProps>();
+const notificationsOpen = defineModel<boolean>("notificationsOpen", { default: false });
 
 const helpBtnDataTestid = computed(() => `${props.dataTestid}-help`);
+const notificationsBtnDataTestid = computed(() => `${props.dataTestid}-notifications`);
+const notificationsMenuDataTestid = computed(() => `${props.dataTestid}-notifications-menu`);
 const userBtnDataTestid = computed(() => `${props.dataTestid}-user`);
 const userMenuDataTestid = computed(() => `${props.dataTestid}-user-menu`);
 
 const showHelp = computed(() => !!props.help);
 const routeProps = useRouteProps(() => props.help);
+
+const showNotifications = computed(() => !!props.notifications);
+const unreadCount = computed(
+  () => props.notifications!.filter((notification) => !notification.read).length,
+);
+const hasUnreadNotifications = computed(() => unreadCount.value > 0);
 
 const showUser = computed(() => !!props.user);
 const userAvatarProps = computed(() =>
@@ -97,5 +145,28 @@ const userAvatarProps = computed(() =>
 
 function isParentOption(option: ParentOptionOrOption): option is ParentOption {
   return "items" in option;
+}
+
+function formatNotificationDate(date: Date) {
+  const interval = toPeriodInterval(date);
+
+  if (wasThisMonth(interval)) {
+    if (wasToday(interval)) return formatDateTime(date, "HH:mm");
+    else if (wasYesterday(interval)) return "Ontem";
+  }
+
+  return formatDateTime(date, "DD/MM/YYYY");
+}
+
+function wasThisMonth(interval: ReturnType<typeof toPeriodInterval>) {
+  return interval.months === 0 && interval.years === 0;
+}
+
+function wasToday(interval: ReturnType<typeof toPeriodInterval>) {
+  return interval.days === 0;
+}
+
+function wasYesterday(interval: ReturnType<typeof toPeriodInterval>) {
+  return interval.days === 1;
 }
 </script>
